499f8672d2fc07113fb72de2ae9cd8cd0bacfdd3
Mark Christian Gabriel <202311074@gorcollege.edu.ph>
2026-04-21 08:33:11 +0800

    App logo inconsistent size in login and register fixed

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo.png') }}" {{ $attributes->merge(['alt' => config('app.name', 'Student Management Portal'), 'class' => 'h-12 w-auto']) }}>
+<img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name', 'Student Management Portal') }}" {{ $attributes }}>
